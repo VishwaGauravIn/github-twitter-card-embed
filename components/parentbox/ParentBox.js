d62e681adf746aea10a00d51a982616494a7b587
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-26 16:50:37 +0530

    added theme support

--- components/parentbox/ParentBox.js
+++ components/parentbox/ParentBox.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
 
 export default function ParentBox() {
   const [darkMode, setDarkMode] = useState(true);
@@ -8,12 +9,22 @@ export default function ParentBox() {
         PREVIEW
       </span>
       <div
-        className={`flex p-8 ring mt-10 rounded ring-cyan-200 ${
+        className={`flex relative ring mt-10 rounded ring-cyan-200 transition-all ease-in-out ${
           darkMode ? `bg-[#010409]` : `bg-white`
         } `}
       >
-        <img src="/api/test" alt="" className="" />
-        <button className=""></button>
+        <img src="/api/test" alt="" className="p-8" />
+        <button
+          className="bg-cyan-200 absolute right-0 -top-8 p-1 flex text-xs items-center gap-1 pl-2 pr-1 rounded-t font-semibold group"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          Theme
+          {darkMode ? (
+            <SunIcon className="w-6 group-active:scale-75 transition-all ease-in-out" />
+          ) : (
+            <MoonIcon className="w-6 group-active:scale-75 transition-all ease-in-out" />
+          )}
+        </button>
       </div>
     </div>
   );
